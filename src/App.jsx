b76524732be5bd1de2084ebc6a527af6413a180a
Evgeniy Pashko <evgpashko92@gmail.com>
2022-02-20 21:00:04 +0700

    Create PostList, training communication between parent and child components using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./styles/App.css";
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList.jsx";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -10,12 +10,7 @@ function App() {
   ]);
   return (
     <div className="App">
-      <h1 style={{ textAlign: "center" }}>
-        Список постов
-      </h1>
-      {posts.map((post) =>
-        <PostItem post={post} key={post.id} />,
-      )}
+      <PostList posts={posts} title="Список постов" />
     </div>
   );
 }
