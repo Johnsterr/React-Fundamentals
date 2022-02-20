ec5d0f36c303aaf51e77e7d0928fc4b1ff8c72eb
Evgeniy Pashko <evgpashko92@gmail.com>
2022-02-20 18:54:48 +0700

    Class and functional components, using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Counter from "./components/Counter.jsx";
+import ClassCounter from "./components/ClassCounter.jsx";
 
 function App() {
   return (
     <div className="App">
-      Hello, world!
+      <Counter />
+      <ClassCounter />
     </div>
   );
 }
