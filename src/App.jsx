4fc0cea987062814a8e6454c07cf70e2121b4a7d
Evgeniy Pashko <evgpashko92@gmail.com>
2022-02-20 22:13:47 +0700

    Two-way data binding with controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,27 @@ function App() {
     { id: 2, title: "CSS", body: "Cascading Style Sheets - каскадные таблицы стилей" },
     { id: 3, title: "JavaScript", body: "JavaScript - язык программирования" },
   ]);
+
+  const [title, setTitle] = useState("");
+
+  const addNewPost = (event) => {
+    event.preventDefault();
+    console.log(title);
+  };
+
   return (
     <div className="App">
-      <Input type="text" placeholder="Название поста" />
-      <Button disabled>Создать пост</Button>
+      <form>
+        {/* Управляемый компонент */}
+        <Input
+          type="text"
+          placeholder="Название поста"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+        <Input type="text" placeholder="Описание поста" />
+        <Button onClick={addNewPost}>Создать пост</Button>
+      </form>
       <PostList posts={posts} title="Список постов" />
     </div>
   );
